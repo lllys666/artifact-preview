1e82c979a5c8439920df8934ddedd9b17a5aba66
liu <140216456+lllys666@users.noreply.github.com>
2023-09-13 01:48:03 -0700

    fix: heroku dynamic port

--- app.js
+++ app.js
@@ -103,4 +103,6 @@ app.get("/download/:id", function (req, res, next) {
   });
 });
 
-app.listen(3000, () => console.log("Listening on port 3000!"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Listening on port 3000!")
+);
